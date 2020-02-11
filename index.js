2129ee8b093e82ea6c62bf213a2e924487ede0c2
jmauro33 <jonathanmauro16@yahoo.com>
2020-02-11 14:49:29 -0600

    updated js

--- index.js
+++ index.js
@@ -36,14 +36,37 @@ axios.get("https://api.github.com/users/"+response.username)
 
 .then(function(github) {
 console.log (github.data)
-writeToFile("profile.txt",JSON.stringify(github.data));
+var neededinfo = {
+    profile_pic: github.data.avatar_url,
+    username: github.data.login,
+    location: github.data.location,
+    bio: github.data.bio,
+    public_repos: github.data.public_repos,
+    followers: github.data.followers,
+    git_stars: github.data.git_stars,
+    following: github.data.following,
+    html_url: github.data.html_url,
+    blog: github.data.blog
+
+}
+writeToFile("profile.txt",JSON.stringify(neededinfo));
 })
 
 })
 
 }
 
-
+//* Profile image
+//* User name
+// * Links to the following:
+//   * User location via Google Maps
+//   * User GitHub profile
+//   * User blog
+// * User bio
+// * Number of public repositories
+// * Number of followers
+// * Number of GitHub stars
+// * Number of users following
 
 
 
