3cf7f54a9921a5ced2ffb32132d4ac17527a6ab3
jmauro33 <jonathanmauro16@yahoo.com>
2020-02-13 18:35:09 -0600

    getting pdf to work

--- index.js
+++ index.js
@@ -1,60 +1,69 @@
-const inquirer = require ("inquirer");
-const axios = require ("axios");
-const fs = require ("fs")
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const pdf = require("html-pdf");
+const generateHTML = require("./generateHTML.js")
 const questions = [
-  
+
 ];
 
 function writeToFile(fileName, data) {
- fs.writeFile(fileName,data,function(err){
-     if (err)console.log (err)
- })
+    fs.writeFile(fileName, data, function (err) {
+        if (err) console.log(err)
+    })
 
 }
 
 function init() {
-inquirer
-    .prompt ([
+    inquirer
+        .prompt([
 
-        {
-            type: "input",
-            message: "Whats your favorite color?",
-            name: "color"
-          },
-        {
-            type: "input",
-            message: "Whats your github username?",
-            name: "username"
-          },
-        
-    ])
+            {
+                type: "input",
+                message: "Whats your favorite color?",
+                name: "color"
+            },
+            {
+                type: "input",
+                message: "Whats your github username?",
+                name: "username"
+            },
 
-.then(function(response) {
+        ])
 
-console.log (response.username)
-axios.get("https://api.github.com/users/"+response.username)
+        .then(function (response) {
 
-.then(function(github) {
-console.log (github.data)
-var neededinfo = {
-    profile_pic: github.data.avatar_url,
-    username: github.data.login,
-    location: github.data.location,
-    bio: github.data.bio,
-    public_repos: github.data.public_repos,
-    followers: github.data.followers,
-    git_stars: github.data.git_stars,
-    following: github.data.following,
-    html_url: github.data.html_url,
-    blog: github.data.blog
+            console.log(response.username)
+            axios.get("https://api.github.com/users/" + response.username)
+                .then(function (github) {
+                    console.log(github.data)
+                    var neededinfo = {
+                        profile_pic: github.data.avatar_url,
+                        username: github.data.login,
+                        location: github.data.location,
+                        bio: github.data.bio,
+                        public_repos: github.data.public_repos,
+                        followers: github.data.followers,
+                        git_stars: github.data.git_stars,
+                        following: github.data.following,
+                        html_url: github.data.html_url,
+                        blog: github.data.blog
+
+                    }
+         writeToFile("profile.txt", JSON.stringify(neededinfo));
+
+                let pdfinfo = generateHTML(neededinfo)
+                pdf.create(pdfinfo).toFile('', function (err, res) {
+                 if (err) return console.log(err);
+                 console.log(res); {'/generateHTML.pdf' }
+                });               
+                })
+
+        })
 
 }
-writeToFile("profile.txt",JSON.stringify(neededinfo));
-})
 
-})
 
-}
 
 //* Profile image
 //* User name
